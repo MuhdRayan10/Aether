acf6681256cc53ca4c7656e76e9f0ec900d30f50
MuhdRayan10 <muhammedrayansavad1@gmail.com>
2026-04-27 16:14:38 +0300

    reorganizing main.cpp into runChatLoop() function

--- main.cpp
+++ main.cpp
@@ -1,19 +1,36 @@
 #include "mySocket.h"
 #include <iostream>
 
+// Function to communicate with the peer in a loop, sending and receiving messages continuously
+void runChatLoop(MySocket& socket) {
+    std::string message;
+    
+    std::cout << "AETHER SESSION (Type 'exit' to quit)" << std::endl;
+
+    // Start listening for messages from peer side
+    socket.startReceiver();
+
+    // As long as socket connection is alive, accept input from user
+    while (socket.isConnected()) {
+        std::cout << "> " << std::flush;
+
+        // If input completes / errors out
+        if (!std::getline(std::cin, message) || message == "exit") break;
+
+        // If message fails to send
+        if (!socket.sendData(message + "\n")) break;
+    }
+
+    // Now, input is completed, wait for responses to finish
+    socket.joinReceiver();
+}
+
 int main() {
 
     MySocket socket;
     socket.connectSocket("127.0.0.1", 8080); // Replace with actual IP and port
-    socket.startReceiver();
+    
+    runChatLoop();
 
-    std::string inputMessage;
-    std::cin >> inputMessage;
-    while (inputMessage != "exit") {
-        if (!socket.sendData(inputMessage)) break;
-        std::cin >> inputMessage;
-    }
-
-    socket.joinReceiver(); 
     return 0;
 }
